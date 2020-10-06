c0980950d4826ef5f4c70d554f44df5eff37aa70
dvramos <dvramos@hotmail.com.br>
2020-10-06 04:28:10 -0300

    4/8 DIO
    function jump

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const dino = document.querySelector ('.dino');
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log('pressionou espaço');
+        jump();
     }
+}
+
+function jump(){
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+            
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <=0){
+                    clearInterval(downInterval);
+                }else{
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);        
+            } else {
+            //subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+            }
+    }, 20);
 }
 document.addEventListener('keyup', handleKeyUp);
